b4e62d65ce4c418998d88db793c62f4d2dd83120
JuljanH <juljan.hoxha@alten.it>
2023-06-08 18:09:38 +0200

    add ViewModel

---
+++ DatabaseUtenti/View Model/Database_Utenti_VM.cs
@@ -0,0 +1,36 @@
+﻿using DatabaseUtenti;
+using LibreriaMFP;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Input;
+
+public class Database_Utenti_VM : INotifyPropertyChanged
+{
+    private string _inputStringa;
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+    protected virtual void OnPropertyChanged(string propertyName)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+    public string InputStringa
+    {
+        get { return _inputStringa; }
+        set
+        {
+            _inputStringa = value;
+            OnPropertyChanged(nameof(InputStringa));
+        }
+    }
+
+    public ICommand PassaStringaCommand { get; }
+    public Database_Utenti_VM()
+    {
+        PassaStringaCommand = new RelayCommand(PassaStringa);
+    }
+    private void PassaStringa()
+    {
+        FilterAppJson.StringFilter(InputStringa, "firstname");
+    }
+}
+
