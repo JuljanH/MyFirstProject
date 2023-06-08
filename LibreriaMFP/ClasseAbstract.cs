c2858d7c56d35847c4efd033b7b787d8ea898084
JuljanH <juljan.hoxha@alten.it>
2023-06-08 18:06:25 +0200

    add abstract class

---
+++ LibreriaMFP/ClasseAbstract.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.ObjectModel;
+
+namespace LibreriaMFP
+{
+    public abstract class Filter_abstract: IDataLoader
+    {
+        public abstract DataLoader LoadData();
+        public abstract void GenderFilter();
+        public abstract void StringFilter(string keyword, string field);
+        public abstract void IdFilter();
+        
+    }
+}
