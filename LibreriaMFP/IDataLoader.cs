c4c6af1ac3188140a2700949e6beabe3063e7d56
JuljanH <juljan.hoxha@alten.it>
2023-06-08 18:06:47 +0200

    implement interface

---
+++ LibreriaMFP/IDataLoader.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace LibreriaMFP
+{
+    public interface IDataLoader
+    {
+        DataLoader LoadData();
+    }
+
+    public class DataLoader
+    {
+
+    }
+}
